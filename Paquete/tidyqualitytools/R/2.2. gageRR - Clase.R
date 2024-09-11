6bdebc589390ed3d47d9d216851537e8be369c81
Fabianenc <milton.encarnacion@epn.edu.ec>
2024-09-11 18:59:48 -0500

    edit

--- Paquete/tidyqualitytools/R/2.2. gageRR - Clase.R
+++ Paquete/tidyqualitytools/R/2.2. gageRR - Clase.R
@@ -27,9 +27,9 @@
 #' @field numM Integer representing the number of measurements per part-operator combination.
 #' @examples
 #' #create gageRR-object
-#' gdo = gageRRDesign(Operators = 3, Parts = 10, Measurements = 3, randomize = FALSE)
+#' gdo <- gageRRDesign(Operators = 3, Parts = 10, Measurements = 3, randomize = FALSE)
 #' #vector of responses
-#' y = c(0.29,0.08, 0.04,-0.56,-0.47,-1.38,1.34,1.19,0.88,0.47,0.01,0.14,-0.80,
+#' y <- c(0.29,0.08, 0.04,-0.56,-0.47,-1.38,1.34,1.19,0.88,0.47,0.01,0.14,-0.80,
 #'       -0.56,-1.46, 0.02,-0.20,-0.29,0.59,0.47,0.02,-0.31,-0.63,-0.46,2.26,
 #'       1.80,1.77,-1.36,-1.68,-1.49,0.41,0.25,-0.11,-0.68,-1.22,-1.13,1.17,0.94,
 #'       1.09,0.50,1.03,0.20,-0.92,-1.20,-1.07,-0.11, 0.22,-0.67,0.75,0.55,0.01,
